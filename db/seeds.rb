412ae2ca9008ddb63cd280148a2589391dd85ab4
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-20 01:22:17 +0000

    Automatically backed up by Learn

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,5 @@ Student.create(name: "Sarah", grade: "10th")
 #Another task you will become familiar with is the seed task. This task is responsible for "seeding" our database with some dummy data.
 
 #The conventional way to seed your database is to have a file in the db directory, db/seeds.rb, that contains some code to create instances of your class.
+
+#Then, we define a rake task that executes the code in this file. This task will also be namespaced under db: See the Rakefile.
